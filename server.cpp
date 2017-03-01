2369ecf9325a8ee95882a029b40821b4d92a44a1
MrAdequate-Bar <NotMao@gmail.com>
2017-02-28 18:42:13 -0800

    Added M3 Functionality
    Priority Queues for packets with timestamps, Artificial latency via a random uniform distribution generator, request timestamp functionality for client and server.

--- server.cpp
+++ server.cpp
@@ -3,7 +3,11 @@ Snake Game for ICS 167 Milestone 3
 
 Notes: There are pre-entered information in the IP, Port, and PlayerName boxes but this can be changed by just clicking the box and editting it. It's this way to make testing quicker.
 
-Andrew Chen, 28676301, andretc1@uci.edu
+Milestone Notes: We add an random artificial latency from 1 to 150ms to sent and received packets. We calculate the latency with the NTP equations from class ((B-A)-(Y-X))/2. The average latencies are between 1 and 300ms. Sometimes we see negative latencies, however, this is when a rollback occurs due to out of order packets. The out of order packets also make the game a little wonky. For example, when the game ends (player dies) the snakes will move backwards to a position closer to the starting position. We do a timestamp request for a latency estimation every second. You can see these estimations on screen and each client's log.
+
+Team Members
+
+Andrew Chen, andretc1@uci.edu, 28676301
 Contributions: 
 
 David Kang, dhkang2@uci.edu, 49958769
@@ -13,7 +17,7 @@ Harry Wong, cheukhw@uci.edu, 66209666
 Contributions: 
 
 Joshua Sosa, jhsosa@uci.edu, 84232577
-Contributions: Modified Periodic Handler to a millisecond count from a count of calls. Setup timestamp priority queues for packets. Added timestamp response functionality on server. Added request timestamp functionality for client. Added Artificial latency with a random normal distribution generator to sent messages. Set up time_loop to request timeStamps every 500ms on client.
+Contributions: Modified Periodic Handler to a millisecond count from a count of calls. Setup timestamp priority queues for packets. Added timestamp response functionality on server. Added request timestamp (time_loop) functionality for client. Added artificial latency in milliseconds to sent and received packets with a random uniform distribution integer generator. Implemented latency calculation and display on client. Modified client logging. 
 
 Please note that we are giving credit to the original sources of the Chatroom Example and Snake Game tutorial followed.
 // Main code for Chatroom Demo taken from 
@@ -22,6 +26,7 @@ Please note that we are giving credit to the original sources of the Chatroom Ex
 // http://thecodeplayer.com/walkthrough/html5-game-tutorial-make-a-snake-game-using-html5-canvas-jquery
 //Timing Code for milliseconds for use in periodic handler taken from http://www.firstobject.com/getmillicount-milliseconds-portable-c++.htm
 */
+
 
 
 #include <stdlib.h>
@@ -289,7 +294,7 @@ void checkQueues(){
 		if(packetsSend.top().first <= getMilliCount()){
 			Packet packet(packetsSend.top().first,packetsSend.top().second);
 			if(packet.second.find("Timestamp,") != string::npos)
-				sendMessage(packet.second + to_string(packet.first));
+				sendMessage(packet.second + to_string(packet.first) + "," + to_string(getMilliCount())); //Timestamp,Xms,Yms 
 			else
 				sendMessage(packet.second);
 			packetsSend.pop();
--- snake.js
+++ snake.js
@@ -1,9 +1,13 @@
-/* 
+/*
 Snake Game for ICS 167 Milestone 3
 
 Notes: There are pre-entered information in the IP, Port, and PlayerName boxes but this can be changed by just clicking the box and editting it. It's this way to make testing quicker.
 
-Andrew Chen, 28676301, andretc1@uci.edu
+Milestone Notes: We add an random artificial latency from 1 to 150ms to sent and received packets. We calculate the latency with the NTP equations from class ((B-A)-(Y-X))/2. The average latencies are between 1 and 300ms. Sometimes we see negative latencies, however, this is when a rollback occurs due to out of order packets. The out of order packets also make the game a little wonky. For example, when the game ends (player dies) the snakes will move backwards to a position closer to the starting position. We do a timestamp request for a latency estimation every second. You can see these estimations on screen and each client's log.
+
+Team Members
+
+Andrew Chen, andretc1@uci.edu, 28676301
 Contributions: 
 
 David Kang, dhkang2@uci.edu, 49958769
@@ -13,7 +17,7 @@ Harry Wong, cheukhw@uci.edu, 66209666
 Contributions: 
 
 Joshua Sosa, jhsosa@uci.edu, 84232577
-Contributions: Modified Periodic Handler to a millisecond count from a count of calls. Setup timestamp priority queues for packets. Added timestamp response functionality on server. Added request timestamp functionality for client. Added Artificial latency with a random normal distribution generator to sent messages. Set up time_loop to request timeStamps every 500ms on client.
+Contributions: Modified Periodic Handler to a millisecond count from a count of calls. Setup timestamp priority queues for packets. Added timestamp response functionality on server. Added request timestamp (time_loop) functionality for client. Added artificial latency in milliseconds to sent and received packets with a random uniform distribution integer generator. Implemented latency calculation and display on client. Modified client logging. 
 
 Please note that we are giving credit to the original sources of the Chatroom Example and Snake Game tutorial followed.
 // Main code for Chatroom Demo taken from 
@@ -22,6 +26,7 @@ Please note that we are giving credit to the original sources of the Chatroom Ex
 // http://thecodeplayer.com/walkthrough/html5-game-tutorial-make-a-snake-game-using-html5-canvas-jquery
 //Timing Code for milliseconds for use in periodic handler taken from http://www.firstobject.com/getmillicount-milliseconds-portable-c++.htm
 */
+
 
 	//Canvas stuff
 	var canvas = $("#canvas")[0];
@@ -52,6 +57,11 @@ Please note that we are giving credit to the original sources of the Chatroom Ex
 	var snake1Length;
 	var snake2Length;
 	var d;
+	
+	//Latency Variables
+	var a;
+	var b;
+	var latency;
   
 	function init()
 	{
@@ -174,7 +184,12 @@ Please note that we are giving credit to the original sources of the Chatroom Ex
 			ctx.fillText(score1_text, 5, h-5);
 			var score2_text = player1Name + "'s Score 1: " + score1;
 			ctx.fillText(score2_text, 5, h-25);
+			var latency_text = "Latency: " + latency;
+			ctx.fillText(latency_text, 5, h-45);
 		}else{
+			ctx.font = "20px Arial";
+			var latency_text = "Latency: " + latency;
+			ctx.fillText(latency_text, 5, h-65);
 			ctx.font = "30px Arial";
 			ctx.fillText("Player Died",5,h-35);
 			ctx.fillText("Press the 'r' key to reset",5,h-5);
@@ -182,7 +197,15 @@ Please note that we are giving credit to the original sources of the Chatroom Ex
 	}
 
 	function timeStamp(){
-		send("Timestamp:")
+		a = new Date().getTime();
+		send("Timestamp:");
+	}
+	
+	function calcLatency(x,y){
+		b = new Date().getTime();
+		//log("clientTime: a = " + a + ", b = " + b);
+		latency = ((b-a)-(y-x))/2;
+		log("Latency: " + latency);
 	}
 	
 	//Lets first create a generic function to paint cells
